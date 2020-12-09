0c51848b76fea2fc99956d87d500aef08d3b1678
limitlesscoders <limitlesscoders@gmail.com>
2020-12-08 20:21:49 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-1rmuc2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-1rmuc2)
---
+++ src/app/demo.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "demo",
+  template: `
+    <h1>Hello {{ name }}!</h1>
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
+})
+export class DemoComponent {}
